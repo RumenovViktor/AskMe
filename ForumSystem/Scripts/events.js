22167fc5461af38eb27eaf4be8fa936a9df04fa0
RumenovViktor <viktorrumenov198@gmail.com>
2015-04-02 23:10:05 +0300

    Add some comments to be clear what am I doing with the comment forms.

--- ForumSystem/Scripts/events.js
+++ ForumSystem/Scripts/events.js
@@ -1,4 +1,6 @@
-﻿$('#open-post-form').click(function () {
+﻿// If the button of the form for asking a question is clicked, a form is show with fadeIn effect and the button hides.
+
+$('#open-post-form').click(function () {
     $('#post')
         .fadeIn("fast");
 
@@ -6,10 +8,15 @@
         .hide();
 });
 
+// Showing the comment for the wanted answer. 
+// Every answer has a link that has his own class that differs him from the other comment links.
+// The same class is set to the form container of every answer.
+// So, if link with class '0commentNo' is clicked, the form container (for the answer) with that class is shown with slideDown effect :).
+
 $('.comment').click(function () {
-    var elementClass = $(this).attr('class');
-    var blankSpaceIndex = elementClass.indexOf(' ');
-    var identifierClass = "";
+    var elementClass = $(this).attr('class'),
+        blankSpaceIndex = elementClass.indexOf(' '),
+        identifierClass = "";
 
     for (var i = blankSpaceIndex + 1; i < elementClass.length; i++) {
         identifierClass += elementClass[i];
